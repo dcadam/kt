e856244a8864646f0b869c0f48c351a23556ad9f
Dillon Adam <dcadam@hku.hk>
2024-07-30 13:58:14 -0500

    Update util.R

--- src/util.R
+++ src/util.R
@@ -434,14 +434,6 @@ generateOffspring<- function(case_data, pair_data, sporadic_logical) {
 }
 
 
-propresponsible <- function(R0, k, prop) {
-  qm1 <- qnbinom(1 - prop, k + 1, mu = R0 * (k + 1) / k)
-  remq <- 1 - prop - pnbinom(qm1 - 1, k + 1, mu = R0 * (k + 1) / k)
-  remx <- remq / dnbinom(qm1, k + 1, mu = R0 * (k + 1) / k)
-  q <- qm1 + 1
-  1 - pnbinom(q - 1, k, mu = R0) - dnbinom(q, k, mu = R0) * remx
-}
-
 
 gen_date_pairs <- function(case_data, pairs_data) {
   a <- case_data %>%
